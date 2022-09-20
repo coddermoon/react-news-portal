3b6d3baf439d54f6214eae8e97125f513796779f
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-09-20 14:51:30 +0600

    gird layout added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,19 @@ function NewsCard() {
   return(
     <div className="newsCard">
 
+<div className="cardBody">
+  <div className="content">
+    <div className="img-container">
+      <img src="image" alt="news tumbnail" />
+    </div>
+    <div className="content-container">
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nulla.</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi illo, ea velit nesciunt odit ex eveniet, laboriosam esse placeat non tempora aliquid blanditiis eos ut aspernatur beatae nobis repudiandae voluptates?</p>
+    </div>
+  
+  </div>
+</div>
+
     </div>
   )
 }
